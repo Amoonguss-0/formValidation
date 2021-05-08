1aa3f9edfd153f68a3bd382b2e89913a3b9e78dd
Justin-F5972 <justinfernandez27@outlook.com>
2021-05-08 16:12:51 -0700

    Added some span messages

--- ts/main.ts
+++ ts/main.ts
@@ -5,8 +5,22 @@ window.onload = function(){
 }
 
 function main():void{
+    resetErrorMessages();
     validateTxtInput("first-name", "First name is required");
     validateTxtInput("last-name", "Last name is required");
+}
+
+function resetErrorMessages(): void{
+    let allSpans = document.querySelectorAll("form span");
+    for( let i = 0; i < allSpans.length; i++){
+        let currSpan = <HTMLElement>allSpans[i];
+        if(currSpan.hasAttribute("data-required")){
+            currSpan.innerText = "*"
+        }
+        else{
+            currSpan.innerText ="";
+        }
+    }
 }
 
 function validateTxtInput(id:string, errMsg:string):boolean{
