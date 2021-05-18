569aac406e783532639838c24768975082e43b39
Justin-F5972 <justinfernandez27@outlook.com>
2021-05-17 18:59:44 -0700

    Added color changing heading and practiced dom

--- js/main.js
+++ js/main.js
@@ -2,14 +2,35 @@ window.onload = function () {
     var formBtn = document.querySelector("form > button");
     formBtn.onclick = main;
 };
+function changeHeading() {
+    var heading = this;
+    var red = Math.floor(Math.random() * 255 + 1);
+    var green = Math.floor(Math.random() * 255 + 1);
+    var blue = Math.floor(Math.random() * 255 + 1);
+    var color = "rgb(" + red + "," + green + "," + blue + ")";
+    heading.style.color = color;
+}
 function main() {
+    var msgHeading = document.createElement("h2");
+    msgHeading.innerText = " processing form";
+    msgHeading.innerText = "processing form";
+    msgHeading.setAttribute("class", "message");
+    msgHeading.onclick = changeHeading;
+    setTimeout(function () {
+        msgHeading.remove();
+    }, 10000);
+    var h1 = document.querySelector("h1");
+    h1.insertAdjacentElement("afterend", msgHeading);
     resetErrorMessages();
     validateTxtInput("first-name", "First name is required");
     validateTxtInput("last-name", "Last name is required");
+    dateValidation();
+}
+function dateValidation() {
     var dobBox = document.getElementById("dob");
     var dob = dobBox.value;
     if (!isValidDate(dob)) {
-        var errorSpan = dobBox.nextElementSibling;
+        var errorSpan = document.getElementById("dob-span");
         errorSpan.innerHTML = "Format should be mm/dd/yyyy";
     }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -3,18 +3,46 @@ window.onload = function(){
     let formBtn = <HTMLElement>document.querySelector("form > button");
     formBtn.onclick = main;
 }
+function changeHeading(){
+    let heading = <HTMLElement>this;
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 + 1)
+    let blue = Math.floor(Math.random() * 255 + 1)
+    let color = "rgb(" + red + "," + green + "," + blue + ")";
+    heading.style.color = color;
+}
 
 function main():void{
+    let msgHeading = document.createElement("h2");
+    msgHeading.innerText = " processing form";
+    msgHeading.innerText = "processing form";
+    msgHeading.setAttribute("class", "message");
+
+    msgHeading.onclick = changeHeading;
+
+    setTimeout(function(){
+        msgHeading.remove();
+    }, 10000)
+
+    let h1 = document.querySelector("h1");
+    h1.insertAdjacentElement("afterend", msgHeading)
+
     resetErrorMessages();
     validateTxtInput("first-name", "First name is required");
     validateTxtInput("last-name", "Last name is required");
 
     //validate date
+    dateValidation();
+}
+
+function dateValidation() {
     let dobBox = <HTMLInputElement>document.getElementById("dob");
     let dob = dobBox.value;
-    if(!isValidDate(dob)){
-        let errorSpan = dobBox.nextElementSibling;
-        errorSpan.innerHTML = "Format should be mm/dd/yyyy"
+    if (!isValidDate(dob)) {
+        //let errorSpan = dobBox.nextElementSibling;
+        //errorSpan.innerHTML = "Format should be mm/dd/yyyy"
+        let errorSpan = document.getElementById("dob-span");
+        errorSpan.innerHTML = "Format should be mm/dd/yyyy";
     }
 }
 
